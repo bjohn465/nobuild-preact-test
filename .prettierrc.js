1e7f493211c4cff597410a466287de35cc76b956
Brandon Johnson <bjohn465+github@gmail.com>
2025-06-01 07:51:14 -0600

    Add Prettier
    Include scripts in `package.json`
    to run some common Prettier commands.

---
+++ .prettierrc.js
@@ -0,0 +1,31 @@
+/** @type {import("prettier").Options} */
+export const config = {
+	arrowParens: 'always',
+	bracketSameLine: false,
+	bracketSpacing: true,
+	embeddedLanguageFormatting: 'auto',
+	endOfLine: 'lf',
+	htmlWhitespaceSensitivity: 'css',
+	insertPragma: false,
+	jsxSingleQuote: false,
+	printWidth: 80,
+	proseWrap: 'always',
+	quoteProps: 'as-needed',
+	requirePragma: false,
+	semi: false,
+	singleAttributePerLine: false,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'all',
+	useTabs: true,
+	overrides: [
+		{
+			files: ['**/package.json', '**/package-lock.json'],
+			options: {
+				useTabs: false,
+			},
+		},
+	],
+}
+
+export default config
